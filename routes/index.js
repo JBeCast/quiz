aea4034c58891589fb594b38512a2b8c4f36707a
JBeCast <jbcsound@gmail.com>
2015-07-07 22:22:12 -0300

    Added first views (main, question, answer) and a single question.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quizController.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizzes/question', quizController.question);
+router.get('/quizzes/answer', quizController.answer);
 
 module.exports = router;
